1eebeadb29fd4ebb08d24dc20a0a7ec3792e3c52
andideve <mikorlk4328@gmail.com>
2020-12-10 19:08:52 +0700

    updates

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const db = require('../db');
-const bodyParser = require('body-parser');
+const db = require("../db");
+const bodyParser = require("body-parser");
 
 const router = express.Router();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 router.get("/users", async (req, res, next) => {
   try {
-    const users = await db.all('users');
+    const users = await db.all("users");
     res.json({
       status: "success",
-      data: users
+      data: users,
     });
   } catch (err) {
     console.log(err);
@@ -20,10 +20,10 @@ router.get("/users", async (req, res, next) => {
 
 router.get("/users/:id", async (req, res, next) => {
   try {
-    const user = await db.one('users', req.params.id);
+    const user = await db.one("users", req.params.id);
     res.json({
       status: "success",
-      data: user
+      data: user,
     });
   } catch (err) {
     console.log(err);
@@ -33,16 +33,16 @@ router.get("/users/:id", async (req, res, next) => {
 
 router.post("/users", urlencodedParser, async (req, res, next) => {
   try {
-    const user = await db.create('users', {
+    const user = await db.create("users", {
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
-      date_created: new Date().getTime()
+      date_created: new Date().getTime(),
     });
     res.json({
       status: "success",
       message: "user has been created",
-      data: user
+      data: user,
     });
   } catch (err) {
     console.log(err);
@@ -52,15 +52,19 @@ router.post("/users", urlencodedParser, async (req, res, next) => {
 
 router.put("/users/:id", urlencodedParser, async (req, res, next) => {
   try {
-    const user = await db.update('users', {
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    }, req.params.id);
+    const user = await db.update(
+      "users",
+      {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      },
+      req.params.id
+    );
     res.json({
       status: "success",
       message: "user has been updated",
-      data: user
+      data: user,
     });
   } catch (err) {
     console.log(err);
@@ -70,11 +74,11 @@ router.put("/users/:id", urlencodedParser, async (req, res, next) => {
 
 router.delete("/users/:id", urlencodedParser, async (req, res, next) => {
   try {
-    const user = await db.delete('users', req.params.id);
+    const user = await db.delete("users", req.params.id);
     res.json({
       status: "success",
       message: "user has been deleted",
-      data: user
+      data: user,
     });
   } catch (err) {
     console.log(err);
